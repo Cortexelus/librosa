e89f8c16b71dec9b5db636706bcb753c9432e9c0
Brian McFee <brian.mcfee@nyu.edu>
2016-05-08 10:07:46 -0400

    purging deprecated features for 0.5 #252

--- librosa/core/__init__.py
+++ librosa/core/__init__.py
@@ -75,14 +75,6 @@ Pitch and tuning
     pitch_tuning
     piptrack
 
-
-Deprecated
-----------
-.. autosummary::
-    :toctree: generated/
-
-    ifptrack
-
 """
 
 from .time_frequency import *  # pylint: disable=wildcard-import
